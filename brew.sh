af7dbc69949323bb4811e15a8d2e9670b73b0ed5
Eric Yang <ericyang879@gmail.com>
2016-12-23 17:13:18 +0800

    update mathiasbynens's bug fix.

--- brew.sh
+++ brew.sh
@@ -41,7 +41,7 @@ brew install wget --with-iri
 # brew install narwhal
 
 # Install more recent versions of some macOS tools.
-brew install vim --override-system-vi
+brew install vim --with-override-system-vi
 brew install homebrew/dupes/grep
 brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
