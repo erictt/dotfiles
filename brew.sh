7846cb3cd1efc265b9eb5d1af5d2c0b66025fdc2
Eric Yang <erictt@users.noreply.github.com>
2019-07-07 23:55:15 -0400

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -23,8 +23,6 @@ brew install findutils
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
 brew install bash
-brew tap homebrew/versions
-brew install bash-completion2
 
 # Switch to using brew-installed bash as default shell
 if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
