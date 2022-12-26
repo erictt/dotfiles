6dfcd50d80ab8b257cac45291bb4705cd1902664
Eric Yang <ericyy87@gmail.com>
2022-12-26 01:50:02 -0500

    use l

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -43,7 +43,7 @@ return {
     "simrat39/symbols-outline.nvim",
     cmd = "SymbolsOutline",
     init = function()
-      vim.keymap.set("n", "<leader>cs", "<cmd>SymbolsOutline<cr>", { desc = "Symbols Outline" })
+      vim.keymap.set("n", "<leader>ls", "<cmd>SymbolsOutline<cr>", { desc = "Symbols Outline" })
     end,
     config = function()
       require("symbols-outline").setup()
