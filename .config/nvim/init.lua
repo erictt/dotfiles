8cc2b092c5ab97905bb38904be166f11d6c704bd
Eric Yang <ericyy87@gmail.com>
2023-01-01 00:49:31 -0500

    reformat the code

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,8 +6,14 @@ require("options")
 -- bootstrap lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", lazypath })
-  vim.fn.system({ "git", "-C", lazypath, "checkout", "tags/stable" }) -- last stable release
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
