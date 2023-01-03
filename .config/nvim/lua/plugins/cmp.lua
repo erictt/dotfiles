f41429878c9817bef954e55fa5160777dd113c84
Eric Yang <eric.yang@lightspeedhq.com>
2023-01-03 17:12:00 -0500

    reconfig

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,113 +1,102 @@
-local cmdline = false
-local M = {
-  "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-emoji",
-    { "hrsh7th/cmp-cmdline", enabled = cmdline },
-    { "dmitmel/cmp-cmdline-history", enabled = cmdline },
-    "hrsh7th/cmp-path",
-    "saadparwaiz1/cmp_luasnip",
-  },
-}
+return {
 
-function M.config()
-  vim.o.completeopt = "menuone,noselect"
-
-  -- Setup nvim-cmp.
-  local cmp = require("cmp")
-
-  cmp.setup({
-    completion = {
-      completeopt = "menu,menuone,noinsert",
-    },
-    snippet = {
-      expand = function(args)
-        require("luasnip").lsp_expand(args.body)
+  -- snippets
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
       end,
     },
-    mapping = cmp.mapping.preset.insert({
-      ["<C-p>"] = cmp.mapping.select_prev_item(),
-      ["<C-n>"] = cmp.mapping.select_next_item(),
-      ["<C-k>"] = cmp.mapping.select_prev_item(),
-      ["<C-j>"] = cmp.mapping.select_next_item(),
-      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      ["<C-Space>"] = cmp.mapping.complete(),
-      ["<C-e>"] = cmp.mapping.close(),
-      ["<CR>"] = cmp.mapping.confirm({
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = false,
-      }),
-      ["<Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        elseif require("luasnip").expand_or_jumpable() then
-          vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-        else
-          fallback()
-        end
-      end, {
-        "i",
-        "s",
-      }),
-      ["<S-Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif require("luasnip").jumpable(-1) then
-          vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-        else
-          fallback()
-        end
-      end, {
-        "i",
-        "s",
-      }),
-    }),
-    sources = cmp.config.sources({
-      { name = "nvim_lsp" },
-      { name = "luasnip" },
-      { name = "buffer" },
-      { name = "path" },
-      { name = "emoji" },
-      { name = "neorg" },
-    }),
-    formatting = {
-      format = require("plugins.lsp.kind").cmp_format(),
+    config = {
+      history = true,
+      delete_check_events = "TextChanged",
     },
-    -- documentation = {
-    --   border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-    --   winhighlight = "NormalFloat:NormalFloat,FloatBorder:TelescopeBorder",
-    -- },
-    experimental = {
-      ghost_text = {
-        hl_group = "LspCodeLens",
-      },
+    init = function()
+      local function jump(key, dir)
+        vim.keymap.set({ "i", "s" }, key, function()
+          return require("luasnip").jump(dir) or key
+        end, { expr = true })
+      end
+      jump("<tab>", 1)
+      jump("<s-tab>", -1)
+    end,
+  },
+
+  -- auto completion
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-emoji",
+      "saadparwaiz1/cmp_luasnip",
     },
-    -- sorting = {
-    --   comparators = {
-    --     cmp.config.compare.sort_text,
-    --     cmp.config.compare.offset,
-    --     -- cmp.config.compare.exact,
-    --     cmp.config.compare.score,
-    --     -- cmp.config.compare.kind,
-    --     -- cmp.config.compare.length,
-    --     cmp.config.compare.order,
-    --   },
-    -- },
-  })
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+          { name = "emoji" },
+        }),
+        formatting = {
+          format = function(_, item)
+            local icons = require("settings").icons.kinds
+            if icons[item.kind] then
+              item.kind = icons[item.kind] .. item.kind
+            end
+            return item
+          end,
+        },
+        experimental = {
+          ghost_text = {
+            hl_group = "LspCodeLens",
+          },
+        },
+      })
+    end,
+  },
 
-  -- cmp.setup.cmdline(":", {
-  --   mapping = cmp.mapping.preset.cmdline(),
-  --   sources = cmp.config.sources({
-  --     -- { name = "noice_popupmenu" },
-  --     { name = "path" },
-  --     { name = "cmdline" },
-  --     -- { name = "cmdline_history" },
-  --   }),
-  -- })
-end
-
-return M
+  -- auto pairs
+  -- {
+  --   "echasnovski/mini.pairs",
+  --   event = "VeryLazy",
+  --   config = function()
+  --     require("mini.pairs").setup({})
+  --   end,
+  -- },
+  --
+  -- -- comments
+  -- { "JoosepAlviste/nvim-ts-context-commentstring" },
+  -- {
+  --   "echasnovski/mini.comment",
+  --   event = "VeryLazy",
+  --   config = function()
+  --     require("mini.comment").setup({
+  --       hooks = {
+  --         pre = function()
+  --           require("ts_context_commentstring.internal").update_commentstring({})
+  --         end,
+  --       },
+  --     })
+  --   end,
+  -- },
+}
