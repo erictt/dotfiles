debc0892bd2044e8c7b52c1b7ee3c432a8af6492
Eric Yang <eric.yang@lightspeedhq.com>
2023-01-03 17:00:19 -0500

    missing configs for mason

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -2,6 +2,8 @@ return {
   "b0o/SchemaStore.nvim", -- used by json lsp for format
   {
     "williamboman/mason.nvim",
+    cmd = "Mason",
+    keys = { { "<leader>lm", "<cmd>Mason<cr>", desc = "Mason" } },
     ensure_installed = {
       "prettierd",
 
@@ -28,6 +30,16 @@ return {
       "pyright", -- python static type checker
       "isort", -- python sort imports
     },
+    config = function(plugin)
+      require("mason").setup()
+      local mr = require("mason-registry")
+      for _, tool in ipairs(plugin.ensure_installed) do
+        local p = mr.get_package(tool)
+        if not p:is_installed() then
+          p:install()
+        end
+      end
+    end,
   },
   {
     "neovim/nvim-lspconfig",
