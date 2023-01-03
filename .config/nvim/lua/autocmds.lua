3ea6503303f20c88514759c007ed0ff0395d3c23
Eric Yang <eric.yang@lightspeedhq.com>
2023-01-03 14:30:01 -0500

    refactor several parts

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -50,22 +50,17 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 })
 
 -- go to last loc when opening a buffer
-vim.api.nvim_create_autocmd("BufReadPre", {
-  pattern = "*",
+vim.api.nvim_create_autocmd("BufReadPost", {
   callback = function()
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "<buffer>",
-      once = true,
-      callback = function()
-        vim.cmd(
-          [[if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$") | exe 'normal! g`"' | endif]]
-        )
-      end,
-    })
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
   end,
 })
 
--- close the window when type q on such file types
+-- close some filetypes with <q>
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = {
     "qf",
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -20,8 +20,6 @@ return {
 
   {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = "BufReadPost",
     dependencies = {
       "nvim-treesitter/nvim-treesitter-textobjects",
     },
@@ -34,6 +32,7 @@ return {
           -- "comment", -- comments are slowing down TS bigtime, so disable for now
           "cpp",
           "diff",
+          -- "gitignore",
           "go",
           "help",
           "html",
@@ -47,15 +46,27 @@ return {
           "lua",
           "markdown",
           "markdown_inline",
+          -- "meson",
+          -- "ninja",
+          -- "nix",
+          -- "norg",
+          -- "org",
           "php",
           "python",
           "query",
           "regex",
           "rust",
+          -- "scss",
+          -- "sql",
+          -- "svelte",
+          -- "teal",
           "toml",
           "tsx",
           "typescript",
+          -- "vhs",
           "vim",
+          -- "vue",
+          -- "wgsl",
           "yaml",
         },
         sync_install = false,
@@ -76,13 +87,6 @@ return {
           enable = true,
           use_virtual_text = true,
           lint_events = { "BufWrite", "CursorHold" },
-        },
-        textsubjects = {
-          enable = true,
-          keymaps = {
-            ["."] = "textsubjects-smart",
-            [";"] = "textsubjects-container-outer",
-          },
         },
         playground = {
           enable = true,
@@ -105,33 +109,15 @@ return {
         textobjects = {
           select = {
             enable = false,
-            lookahead = true,
-            keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-            },
           },
           move = {
             enable = false,
-            set_jumps = true, -- whether to set jumps in the jumplist
-            goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
-            goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
-            goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
-            goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
           },
           lsp_interop = {
             enable = false,
-            peek_definition_code = {
-              ["gD"] = "@function.outer",
-            },
           },
         },
       })
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      parser_config.markdown.filetype_to_parsername = "octo"
     end,
   },
 }
