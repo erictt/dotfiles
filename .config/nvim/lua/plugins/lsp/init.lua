de9ce11b2fcfbf5eb6597a1ee75583d3ebd62fb5
Eric Yang <eric.yang@lightspeedhq.com>
2023-01-05 16:59:41 -0500

    move out mason configs

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -1,3 +1,5 @@
+local configs = require("plugins.lsp.configs")
+
 return {
   "jose-elias-alvarez/typescript.nvim",
   "b0o/SchemaStore.nvim", -- used by json lsp for format
@@ -5,33 +7,7 @@ return {
     "williamboman/mason.nvim",
     cmd = "Mason",
     -- keys = { { "<leader>lm", "<cmd>Mason<cr>", desc = "Mason" } },
-    ensure_installed = {
-      "prettierd",
-
-      "lua-language-server", -- lua lsp
-      "stylua", -- lua formatter
-      "selene",
-      "luacheck", -- lua
-
-      "shellcheck", -- shell script analyser
-      "shfmt", -- shell format
-
-      "eslint_d", -- javascript, typescript linkt
-      "deno", -- javascript, typescript defaults
-
-      "clang-format", -- c, cpp
-      "clangd", -- c, cpp
-
-      "intelephense", -- php lsp
-      "phpstan", -- php lint
-      "php-cs-fixer", -- php formatter
-
-      "black", -- python formatter
-      "yapf", -- python formatter
-      "pylint", --python static code analyser
-      "pyright", -- python static type checker
-      "isort", -- python sort imports
-    },
+    ensure_installed = configs.mason,
     config = function(plugin)
       require("mason").setup()
       local mr = require("mason-registry")
@@ -79,7 +55,7 @@ return {
       vim.api.nvim_command("hi DiagnosticInfo guifg=" .. colors.info)
 
       -- lspconfig
-      local servers = require("plugins.lsp.servers")
+      local servers = configs.lsp
       local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
       require("mason-lspconfig").setup({ ensure_installed = vim.tbl_keys(servers) })
