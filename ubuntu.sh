09647c11d8705c0e8998f6513c9d0ab0dc383665
Eric Yang <eric.yang@lightspeedhq.com>
2023-01-25 13:17:36 -0500

    configure zsh for ubuntu/debian

---
+++ ubuntu.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get upgrade -y
+
+DEBIAN_FRONTEND=noninteractive apt-get -y install \
+	build-essential \
+	golang \
+	python3-venv \
+	python3-pandas \
+	python3-matplotlib \
+	tmux \
+	git \
+	zip \
+	unzip \
+	npm \
+	nodejs
+sqlite3 \
+	sqlite3-doc
