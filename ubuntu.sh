5941e228d233c0228d2b475ff586909fd9a6d978
Eric Yang <eric.yang@lightspeedhq.com>
2023-01-28 04:26:27 +0000

    manually install golang/nodejs/npm/neovim

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
+cd /tmp/ || exit
+
 apt-get update
 apt-get upgrade -y
 
 DEBIAN_FRONTEND=noninteractive apt-get -y install \
 	build-essential \
-	golang \
 	python3-venv \
 	python3-pandas \
 	python3-matplotlib \
@@ -13,11 +14,23 @@ DEBIAN_FRONTEND=noninteractive apt-get -y install \
 	git \
 	zip \
 	unzip \
-	npm \
-	nodejs \
 	sqlite3 \
 	sqlite3-doc
 
+echo "=============================== Install latest nodejs ==============================="
+NODE_VERSION=19.x
+curl -fsSL https://deb.nodesource.com/setup_$NODE_VERSION | sudo -E bash - &&
+	sudo apt-get install -y nodejs npm
+
+echo "=============================== Install latest golang ==============================="
+GO_VERSION=1.19.5
+rm -rf /usr/bin/go /usr/lib/go /usr/local/go /usr/share/go
+wget https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz
+tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
+ln -sf /usr/local/go/bin/go /usr/bin/go
+ln -sf /usr/local/go/bin/gofmt /usr/bin/gofmt
+
+echo "=============================== Install latest neovim ==============================="
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 mv nvim.appimage /etc/alternatives/nvim.appimage
