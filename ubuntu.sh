d9a7bc9f5b3567ac3a70d7ef83c0019b965ebc76
Eric Yang <ericyy87@gmail.com>
2023-02-02 02:25:20 +0000

    add script for install lazygit

--- ubuntu.sh
+++ ubuntu.sh
@@ -39,3 +39,9 @@ mv nvim.appimage /etc/alternatives/nvim.appimage
 
 ln -sf /etc/alternatives/nvim.appimage /usr/bin/vi
 ln -sf /etc/alternatives/nvim.appimage /usr/bin/nvim
+
+echo "=============================== Install latest Lazygit ==============================="
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
