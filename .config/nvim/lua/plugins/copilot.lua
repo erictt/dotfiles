681def4c6526ff150b184e7626344ef233749b8a
Eric Yang <eric.yang@lightspeedhq.com>
2023-02-04 12:11:36 -0500

    bring back copilot for typescript one line issue

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -7,9 +7,53 @@ return {
       "zbirenbaum/copilot.lua",
     },
     config = function()
+      -- require("copilot").setup({
+      --   suggestion = { enabled = false },
+      --   panel = { enabled = false },
+      -- })
+
       require("copilot").setup({
-        suggestion = { enabled = false },
-        panel = { enabled = false },
+        panel = {
+          enabled = false,
+          -- auto_refresh = false,
+          -- keymap = {
+          --   jump_prev = "[[",
+          --   jump_next = "]]",
+          --   accept = "<CR>",
+          --   refresh = "gr",
+          --   open = "<M-CR>",
+          -- },
+          -- layout = {
+          --   position = "bottom", -- | top | left | right
+          --   ratio = 0.4,
+          -- },
+        },
+        suggestion = {
+          enabled = true,
+          auto_trigger = false,
+          debounce = 75,
+          keymap = {
+            accept = "<M-l>",
+            accept_word = false,
+            accept_line = false,
+            next = "<M-j>",
+            prev = "<M-k>",
+            dismiss = "<M-;>",
+          },
+        },
+        filetypes = {
+          yaml = false,
+          markdown = false,
+          help = false,
+          gitcommit = false,
+          gitrebase = false,
+          hgcommit = false,
+          svn = false,
+          cvs = false,
+          ["."] = false,
+        },
+        copilot_node_command = "node", -- Node.js version must be > 16.x
+        server_opts_overrides = {},
       })
 
       local has_words_before = function()
