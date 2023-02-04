019cf8df3ab89647da3613067279142f00e40f2a
Eric Yang <eric.yang@lightspeedhq.com>
2023-02-04 12:05:04 -0500

    reconfig copilot cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -90,6 +90,7 @@ return {
           }),
         }),
         sources = cmp.config.sources({
+          { name = "copilot" },
           { name = "nvim_lsp" },
           { name = "luasnip" },
           { name = "buffer" },
