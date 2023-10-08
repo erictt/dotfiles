5b01b8a41b7da928de276479246921a17b27ac26
Eric Yang <ericyy87@gmail.com>
2023-10-08 15:02:59 -0400

    disable notify

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,28 +1,27 @@
 return {
-  "MunifTanjim/nui.nvim",
   { "nvim-tree/nvim-web-devicons", config = { default = true } },
-  {
-    "rcarriga/nvim-notify",
-    enabled = false,
-    keys = {
-      {
-        "<leader>md",
-        function()
-          require("notify").dismiss({ silent = true, pending = true })
-        end,
-        desc = "Delete all Notifications",
-      },
-    },
-    config = {
-      timeout = 3000,
-      max_height = function()
-        return math.floor(vim.o.lines * 0.75)
-      end,
-      max_width = function()
-        return math.floor(vim.o.columns * 0.75)
-      end,
-    },
-  },
+  -- {
+  --   "rcarriga/nvim-notify",
+  --   enabled = false,
+  --   keys = {
+  --     {
+  --       "<leader>md",
+  --       function()
+  --         require("notify").dismiss({ silent = true, pending = true })
+  --       end,
+  --       desc = "Delete all Notifications",
+  --     },
+  --   },
+  --   config = {
+  --     timeout = 3000,
+  --     max_height = function()
+  --       return math.floor(vim.o.lines * 0.75)
+  --     end,
+  --     max_width = function()
+  --       return math.floor(vim.o.columns * 0.75)
+  --     end,
+  --   },
+  -- },
 
   -- better vim.ui
   {
@@ -72,46 +71,50 @@ return {
   },
 
   -- noicer ui
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    config = {
-      lsp = {
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-        progress = {
-          enabled = true,
-          -- format = "conceal",
-          -- format = "lsp_progress",
-          -- format_done = "conceal",
-          -- format_done = "lsp_progress_done",
-          throttle = 1000, -- frequency to update lsp progress message (1000ms)
-          view = "mini",
-        },
-        -- message = {
-        --   enabled = false,
-        -- },
-      },
-      presets = {
-        bottom_search = true,
-        command_palette = true,
-        long_message_to_split = true,
-      },
-    },
-    throttle = 100,
-    -- stylua: ignore
-    keys = {
-      -- { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
-      { "<leader>ml", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
-      { "<leader>mh", function() require("noice").cmd("history") end, desc = "Noice History" },
-      { "<leader>ma", function() require("noice").cmd("all") end, desc = "Noice All" },
-      { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true },
-      { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true },
-    },
-  },
+  -- {
+  --   "folke/noice.nvim",
+  --   event = "VeryLazy",
+  --   dependencies = {
+  --     "MunifTanjim/nui.nvim",
+  --     -- "rcarriga/nvim-notify",
+  --   },
+  --   config = {
+  --     lsp = {
+  --       override = {
+  --         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+  --         ["vim.lsp.util.stylize_markdown"] = true,
+  --         ["cmp.entry.get_documentation"] = true,
+  --       },
+  --       progress = {
+  --         enabled = true,
+  --         -- format = "conceal",
+  --         -- format = "lsp_progress",
+  --         -- format_done = "conceal",
+  --         -- format_done = "lsp_progress_done",
+  --         throttle = 1000, -- frequency to update lsp progress message (1000ms)
+  --         view = "mini",
+  --       },
+  --       -- message = {
+  --       --   enabled = false,
+  --       -- },
+  --     },
+  --     presets = {
+  --       bottom_search = true,
+  --       command_palette = true,
+  --       long_message_to_split = true,
+  --     },
+  --   },
+  --   throttle = 100,
+  --   -- stylua: ignore
+  --   keys = {
+  --     -- { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
+  --     { "<leader>ml", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
+  --     { "<leader>mh", function() require("noice").cmd("history") end, desc = "Noice History" },
+  --     { "<leader>ma", function() require("noice").cmd("all") end, desc = "Noice All" },
+  --     { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true },
+  --     { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true },
+  --   },
+  -- },
 
   -- colorizer
   {
