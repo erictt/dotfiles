1105caadd3f3257fc07f488dc60ad595bb4e79a8
Eric Yang <ericyy87@gmail.com>
2025-01-25 00:28:32 -0500

    continue to update neovim

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -46,4 +46,15 @@ return {
       },
     },
   },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+      },
+    },
+  },
 }
